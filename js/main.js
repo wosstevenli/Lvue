72d5f760d7634be159bdff17f83df15f630dbeb2
STEVEN LI <wosstevenli@gmail.com>
2017-05-29 15:18:27 +0800

    Vue起步第一页

--- js/main.js
+++ js/main.js
@@ -26,3 +26,59 @@ var app4 = new Vue({
     seen:false
   }
 })
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    todos: [
+      { text: '权力的游戏' },
+      { text: '冰与火之歌' },
+      { text: '异鬼' }
+    ]
+  }
+})
+
+// 处理用户输入
+var app6 = new Vue({
+  el:'#app-6',
+  data:{
+    message:"Hello Google!"
+  },
+  methods:{
+    reversemessage:function(){
+      this.message=this.message.split('').reverse().join('') //完整动作将message分拆，反向排列，组合成字符串
+      //this.message=this.message.split('')  //将message分拆成字符加上''
+      //this.message=this.message.split('').reverse() //将message字符反向排列
+      console.log(typeof 'message')
+    }
+  }
+})
+
+//双向数据绑定
+// js中严格区分大小写Vue
+var app7=new Vue({
+  el:'#app-7',
+  data:{
+    message:'GOOGLE MAP'
+  }
+})
+
+// 定义Vue组件
+Vue.component('todo-item', {
+  // todo-item 组件现在接受一个
+  // "prop"，类似于一个自定义属性
+  // 这个属性名为 todo。
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app8 = new Vue({
+  el: '#app-8',
+  data: {
+    groceryList: [
+      { text: '蔬菜' },
+      { text: '奶酪' },
+      { text: '随便其他什么人吃的东西' }
+    ]
+  }
+})
